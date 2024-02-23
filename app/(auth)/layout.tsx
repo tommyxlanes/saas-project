77797eb75473410a2eb2852ff5cebabf459c8f8f
tommyxlanes <tommyxlanes@gmail.com>
2024-02-23 14:27:02 -0800

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const layout = ({ 
+    children 
+}: {
+    children: React.ReactNode
+}) => {
+  return (
+    <main className='auth'>
+        {children}
+    </main>
+  )
+}
+
+export default layout
